8de6759fa3ac25bf1f834a09c1c751b10d484914
Maki <mxmcube@gmail.com>
2020-06-15 04:01:41 +0100

    Updated WebRTC to work with macOS

--- cmake/ports/webrtc/portfile.cmake
+++ cmake/ports/webrtc/portfile.cmake
@@ -1,6 +1,8 @@
 include(vcpkg_common_functions)
-set(WEBRTC_VERSION m84.6.0)
+set(WEBRTC_VERSION 84.6.0)
 set(MASTER_COPY_SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src)
+
+set(WEBRTC_DOWNLOAD_URL https://github.com/crow-misia/libwebrtc-bin/releases/download)
 
 if (ANDROID)
    # this is handled by hifi_android.py
@@ -14,15 +16,15 @@ elseif (WIN32)
 elseif (APPLE)
     vcpkg_download_distfile(
         WEBRTC_SOURCE_ARCHIVE
-        URLS https://cdn.tivolicloud.com/dependencies/webrtc-m78-osx.tar.gz
-        SHA512 8b547da921cc96f5c22b4253a1c9e707971bb627898fbdb6b238ef1318c7d2512e878344885c936d4bd6a66005cc5b63dfc3fa5ddd14f17f378dcaa17b5e25df
-        FILENAME webrtc-m78-osx.tar.gz
+        URLS ${WEBRTC_DOWNLOAD_URL}/${WEBRTC_VERSION}/libwebrtc-macox-x64.tar.xz
+        SHA512 f66b15d098a2b4401ee65ed9146b5b4fb4906fef085c853bce38cb422b198b90d49e1fbc597e0816cf4436451bd6817e98579f44ca16bc500de78bb1d08ee284
+        FILENAME libwebrtc-macox-x64.tar.xz
     )
 else ()
     # else Linux desktop
     vcpkg_download_distfile(
         WEBRTC_SOURCE_ARCHIVE
-        URLS https://github.com/crow-misia/libwebrtc-bin/releases/download/84.6.0/libwebrtc-linux-x64.tar.xz
+        URLS ${WEBRTC_DOWNLOAD_URL}/${WEBRTC_VERSION}/libwebrtc-linux-x64.tar.xz
         SHA512 df71748b8bc78d776494d46e99f06438b9917c8ee39b521f1506b6de609b1af1f3019eba22cfc4f5da14255dc70ec26acbb6a7878899ee8758809a9759af3904
         FILENAME libwebrtc-linux-x64.tar.xz
     )
@@ -30,8 +32,13 @@ endif ()
 
 vcpkg_extract_source_archive(${WEBRTC_SOURCE_ARCHIVE})
 
-file(COPY ${MASTER_COPY_SOURCE_PATH}/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include/webrtc)
-file(COPY ${MASTER_COPY_SOURCE_PATH}/lib/libwebrtc.a DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(COPY ${MASTER_COPY_SOURCE_PATH}/lib/libwebrtc.a DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-
-file(INSTALL ${MASTER_COPY_SOURCE_PATH}/NOTICE DESTINATION ${CURRENT_PACKAGES_DIR}/share/webrtc RENAME copyright)
+if (ANDOIRD)
+    # this is handled by hifi_android.py
+elseif (WIN32)
+    # TODO: slightly different file structure
+else ()
+    file(COPY ${MASTER_COPY_SOURCE_PATH}/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include/webrtc)
+    file(COPY ${MASTER_COPY_SOURCE_PATH}/lib/libwebrtc.a DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+    file(COPY ${MASTER_COPY_SOURCE_PATH}/lib/libwebrtc.a DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+    file(INSTALL ${MASTER_COPY_SOURCE_PATH}/NOTICE DESTINATION ${CURRENT_PACKAGES_DIR}/share/webrtc RENAME copyright)
+endif()
